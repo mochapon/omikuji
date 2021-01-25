e2117d397d81db1ae9067931905c0407fc36d5ea
mochapon <77262390+mochapon@users.noreply.github.com>
2021-01-25 21:41:20 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,12 +8,12 @@
     // const results = ['大吉', '中吉', '凶', '末吉'];
     // btn.textContent = results[Math.floor(Math.random() * results.length)];
     const n = Math.random();
-    if (n < 0.05) {
-      btn.textContent = '大吉'; // 5%
-    } else if (n < 0.2) {
-      btn.textContent = '中吉'; // 15%
+    if (n < 0.1) {
+      btn.textContent = '中吉'; // 10%
+    } else if (n < 0.4) {
+      btn.textContent = '大吉'; // 40%
     } else {
-      btn.textContent = '凶'; // 80%
+      btn.textContent = '凶'; // 50%
     }
   });
 }
